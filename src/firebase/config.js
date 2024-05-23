7e29f223d8a0a08acac48b39d4c2423bb07eaab3
alicia-castillo <alicia-castilloo@outlook.com>
2024-05-23 14:11:39 -0600

    secrets in

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,12 +7,7 @@ import { getFirestore } from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBuIR4TGXK2P9jlnwyTsLOAUFwWcMyDYdo",
-  authDomain: "react-cursos-3d491.firebaseapp.com",
-  projectId: "react-cursos-3d491",
-  storageBucket: "react-cursos-3d491.appspot.com",
-  messagingSenderId: "328154639022",
-  appId: "1:328154639022:web:826c6cb460761773dc1b3f"
+  
 };
 
 // Initialize Firebase
